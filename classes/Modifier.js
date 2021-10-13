a65bcf9974193cbf85bc0876efed7e61a1671bb7
Nate <natediven1@gmail.com>
2021-10-13 03:05:44 -0600

    Cleaned up code a little bit and made small UI changes

--- classes/Modifier.js
+++ classes/Modifier.js
@@ -1,7 +1,7 @@
 //
 //
 class Modifier {
-	// base modifier class
+	// base modifier class that doesn't do anything, but is the foundation for other modifiers
 
 	// name that should match what is on the lookup table in Filemod.addModifier method
 	name = 'modifier';
@@ -13,7 +13,11 @@ class Modifier {
 		// assign uuid
 		this.uuid = this.generateUuid();
 
+		// when true the modifier html should set to display: none
 		this.minimized = false;
+
+		// true when the modifier is being executed
+		this.isActive = false;
 
 		// define defaults
 		this.state = this.defaults();
@@ -31,40 +35,44 @@ class Modifier {
 		return {};
 	}
 
-	onLoad() {}
+	onLoad() {
+		// called after a modifier is loaded in
+	}
 
 	preview(fileList) {
 		// returns a preview of what the modified file list would look like, but dosn't apply modifications
-
 		return fileList;
 	}
 
 	async modify(fileList) {
 		// modifys and returns fileList
-
 		return fileList;
 	}
 
 	render() {
 		// returns html for the modifier options
-
 		return "<p>This modifier doesn't do anything</p>";
 	}
 
 	postRender() {}
 
 	async getDirectoryFromUser() {
+		// triggers file dialog for user to select a folder and returns path of it
+		// so far it has only worked on Window
+		// tested on MacOS but it did not work
 		let val = await fileDialog({ type: 'directory' });
 		return val[0];
 	}
 	async getFileFromUser() {
+		// triggers file dialog for user to select a file and returns path of it
+		// so far it has only worked on Window
+		// tested on MacOS but it did not work
 		let val = await fileDialog({ type: 'open-file' });
 		return val[0];
 	}
 
 	generateUuid() {
 		// rerurns a new uuid
-
 		return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
 			var r = (Math.random() * 16) | 0,
 				v = c == 'x' ? r : (r & 0x3) | 0x8;
@@ -74,34 +82,57 @@ class Modifier {
 
 	rerenderModifiers() {
 		// calls rerender event
-
 		const event = new Event('render modifiers');
 		document.dispatchEvent(event);
 	}
 
 	rerenderPreview() {
 		// calls rerender event
-
 		const event = new Event('render preview');
 		document.dispatchEvent(event);
 	}
 
 	getFileName(filePath) {
 		// returns the file name from the file path
-
 		return path.basename(filePath);
 	}
 
 	getFileType(filePath) {
 		// returns the file type (based off file extension)
-
 		return path.extname(filePath);
 	}
 
 	getFileDir(filePath) {
 		// returns the directory the file is in
-
 		return path.dirname(filePath);
+	}
+
+	moveFilePreview(filePath, dir) {
+		let newPath = path.join(dir, this.getFileName(filePath));
+		return newPath;
+	}
+
+	async moveFile(filePath, dir) {
+		let newPath = path.join(dir, this.getFileName(filePath));
+
+		if (newPath != filePath) {
+			try {
+				try {
+					fs.readFileSync(newPath);
+					console.log('file not moved because file already exists with that name');
+				} catch (error) {
+					move(filePath, this.getFileDir(newPath), (error) => {
+						if (error) console.log(error);
+					});
+					return newPath;
+				}
+			} catch (error) {
+				console.log('could not rename file', error);
+				return filePath;
+			}
+		}
+
+		return filePath;
 	}
 
 	renameFilePreview(filePath, name) {
@@ -110,27 +141,6 @@ class Modifier {
 		let ext = this.getFileType(filePath);
 
 		return dir + '/' + name + ext;
-	}
-
-	saveState() {}
-
-	getJSON() {
-		// returns modifier saved as a JSON object
-		let a = {
-			type: this.name,
-			state: this.state,
-		};
-
-		return JSON.stringify(a);
-	}
-
-	deleteFile(filePath) {
-		console.log(filePath);
-		try {
-			fs.unlinkSync(filePath);
-		} catch (error) {
-			console.log(`${filePath} could not be deleted`, error);
-		}
 	}
 
 	renameFile(filePath, name) {
@@ -157,32 +167,23 @@ class Modifier {
 		return filePath;
 	}
 
-	async moveFile(filePath, dir) {
-		let newPath = path.join(dir, this.getFileName(filePath));
-
-		if (newPath != filePath) {
-			try {
-				try {
-					fs.readFileSync(newPath);
-					console.log('file not moved because file already exists with that name');
-				} catch (error) {
-					move(filePath, this.getFileDir(newPath), (error) => {
-						if (error) console.log(error);
-					});
-					return newPath;
-				}
-			} catch (error) {
-				console.log('could not rename file', error);
-				return filePath;
-			}
+	deleteFile(filePath) {
+		// deletes file with given path
+		try {
+			fs.unlinkSync(filePath);
+		} catch (error) {
+			console.log(`${filePath} could not be deleted`, error);
 		}
-
-		return filePath;
 	}
 
-	moveFilePreview(filePath, dir) {
-		let newPath = path.join(dir, this.getFileName(filePath));
-		return newPath;
+	getJSON() {
+		// returns modifier saved as a JSON object
+		let a = {
+			type: this.name,
+			state: this.state,
+		};
+
+		return JSON.stringify(a);
 	}
 
 	textToRegex(s) {
--- classes/TorrentModifier.js
+++ classes/TorrentModifier.js
@@ -1,10 +1,13 @@
 //
-
 //
 class TorrentModifier extends Modifier {
+	// modifier that downloads torrent files
+
 	name = 'torrentModifier';
 	title = 'Torrent Download';
 
+	// 	GENERAL FUNCTIONS
+	// 	GENERAL FUNCTIONS
 	defaults() {
 		return {
 			filePath: '',
@@ -15,176 +18,20 @@ class TorrentModifier extends Modifier {
 		};
 	}
 
-	render() {
-		let text = `
-		<div class="input">
-        <p class="description">Downloads a torrent</p>
-		</div>
-        <div class="input">
-        <p class="directory" id="filePath-${this.uuid}">${
-			this.state.filePath === '' ? 'Select Torrent File' : this.state.filePath
-		}</p>
-        <p class="description">torrent file path<p>
-        </div>
-        <div class="input">
-        <p class="directory" id="directory-${this.uuid}">${
-			this.state.destination === '' ? 'Select Torrent File' : this.state.destination
-		}</p>
-        <p class="description">torrent download destination<p>
-        </div>
-        `;
-
-		if (this.state.files.length > 0) {
-			text += `<div class="input"><p>Torrent files</p>
-			<p class="description">file that are going to be downloaded<p>
-			</div>
-			<div class="input">
-			`;
-
-			this.state.files.forEach((v) => {
-				const isChecked = this.state.filteredFiles.findIndex((file) => file == v) < 0;
-
-				// text += `
-				// <div>
-				// <input type="checkbox" name="check-${v}-${this.uuid}" id="check-${v}-${
-				// 	this.uuid
-				// }" ${isChecked ? 'checked' : ''} />
-				// <label for="check-${v}-${this.uuid}">${v}</label>
-
-				// </div>
-				// `;
-				text += `
-				<p>${this.getFileName(v)}</p>
-				`;
-			});
-
-			text += `</div>`;
-		}
-
-		if (this.stats.inProgress) {
-			let progress = Number(this.stats.progress);
-			progress *= 10000;
-			progress = Math.round(progress);
-			progress /= 100;
-
-			let speed = this.humanSpeed(this.stats.speed);
-			console.log(speed);
-
-			let timeLeft = this.parseMillisecondsIntoReadableTime(this.stats.eta);
-			text += `
-			<p>Progress: ${progress}%</p>
-			<p>Speed: ${speed}</p>
-			<p>Time left: ${timeLeft}</p>
-			`;
-		}
-
-		return text;
-	}
-
-	postRender() {
-		const filePath = document.getElementById(`filePath-${this.uuid}`);
-		const directory = document.getElementById(`directory-${this.uuid}`);
-		filePath.addEventListener('click', async (event) => {
-			try {
-				console.log('getting file');
-				this.state.filePath = await this.getFileFromUser();
-				this.state.files = this.state.filteredFiles = [];
-				this.onLoad();
-			} catch (error) {}
-			this.rerenderModifiers();
-		});
-
-		// this.state.files.forEach((v) => {
-		// 	let checkbox = document.getElementById(`check-${v}-${this.uuid}`);
-		// 	checkbox.addEventListener('input', (event) => {
-		// 		let value = event.target.checked;
-
-		// 		if (!value) {
-		// 			this.state.filteredFiles.push(v);
-		// 		} else {
-		// 			let index = this.state.filteredFiles.findIndex((file) => file == v);
-		// 			console.log(index);
-		// 			if (index >= 0) {
-		// 				console.log('removing', index);
-		// 				this.state.filteredFiles.splice(index, 1);
-		// 			}
-		// 		}
-
-		// 		console.log(value);
-		// 	});
-		// });
-
-		directory.addEventListener('click', async (event) => {
-			try {
-				console.log('getting folder');
-				this.state.destination = await this.getDirectoryFromUser();
-				this.onLoad();
-			} catch (error) {}
-			this.rerenderModifiers();
-		});
-	}
-
-	async onLoad() {
-		if (this.state.filePath === '' || this.state.destination === '') return;
-
-		try {
-			const client = new WebTorrent();
-			const torrentString = fs.readFileSync(this.state.filePath, {
-				path: this.state.destination,
-			});
-			const torrent = await client.add(torrentString, { path: this.state.destination });
-			console.log('this is the torrent', torrent);
-			this.state.files = torrent.files.map((v) =>
-				path.join(this.state.destination, v.path)
-			);
-			console.log(this.state.files);
-			client.destroy();
-		} catch (error) {}
-
-		this.rerenderModifiers();
-	}
-	stats = {
-		inProgress: false,
-		progress: 0,
-		speed: 0,
-		eta: 0,
-	};
-
 	async modify(fileList) {
+		// starts torrent download and periodically updates the ui to show progress
+		// returns fileList with added torrent files when promise is done
 		if (this.state.filePath === '' || this.state.destination === '') return fileList;
-		this.stats.inProgress = true;
+		this.rerenderModifiers();
+
 		let newList = fileList;
 		newList.push(...this.state.files);
 
 		try {
-			this.isDownloading = true;
+			this.stats.inProgress = true;
 			const client = new WebTorrent();
 			const torrentFile = fs.readFileSync(this.state.filePath);
-			const torrent = client.add(
-				torrentFile,
-				{ path: this.state.destination }
-				// (torrent) => {
-				// 	let length = torrent.files.length;
-
-				// 	torrent.files.forEach((file) => {
-				// 		const source = file.createReadStream();
-				// 		const destination = fs.createWriteStream(
-				// 			path.join(this.state.destination, file.name)
-				// 		);
-
-				// 		source.on('end', () => {
-				// 			length -= 1;
-				// 			console.log('file ended');
-
-				// 			if (!length <= 0) {
-				// 				console.log('finished downloading');
-				// 			} else {
-				// 				console.log('still more files');
-				// 			}
-				// 		});
-				// 	});
-				// }
-			);
+			const torrent = client.add(torrentFile, { path: this.state.destination });
 
 			return new Promise((resolve, reject) => {
 				let interval = setInterval(() => {
@@ -192,7 +39,6 @@ class TorrentModifier extends Modifier {
 					this.stats.eta = torrent.timeRemaining;
 					this.stats.progress = torrent.progress;
 					this.stats.speed = torrent.downloadSpeed;
-					console.log(this.stats);
 					this.rerenderModifiers();
 					if (torrent.done) {
 						client.destroy();
@@ -221,6 +67,7 @@ class TorrentModifier extends Modifier {
 	}
 
 	preview(fileList) {
+		// adds torrent files to the preview fileList
 		let newList = fileList;
 
 		if (this.state.files.length > 0) {
@@ -232,7 +79,94 @@ class TorrentModifier extends Modifier {
 		return newList.sort();
 	}
 
+	// RENDER FUNCTIONS
+	// RENDER FUNCTIONS
+	render() {
+		let text = `
+		<div class="input">
+        <p class="description">Downloads a torrent</p>
+		</div>
+        <div class="input">
+        <p class="directory" id="filePath-${this.uuid}">${
+			this.state.filePath === '' ? 'Select Torrent File' : this.state.filePath
+		}</p>
+        <p class="description">torrent file path<p>
+        </div>
+        <div class="input">
+        <p class="directory" id="directory-${this.uuid}">${
+			this.state.destination === '' ? 'Select Torrent File' : this.state.destination
+		}</p>
+        <p class="description">torrent download destination<p>
+        </div>
+        `;
+
+		// render file names if any
+		if (this.state.files.length > 0) {
+			text += `<div class="input"><p>Torrent files</p>
+			<p class="description">file that are going to be downloaded<p>
+			</div>
+			<div class="input">
+			`;
+
+			this.state.files.forEach((v) => {
+				const isChecked = this.state.filteredFiles.findIndex((file) => file == v) < 0;
+				text += `
+				<p>${this.getFileName(v)}</p>
+				`;
+			});
+
+			text += `</div>`;
+		}
+
+		// render stats if the torrent is donwloading
+		if (this.stats.inProgress) {
+			let progress = Number(this.stats.progress);
+			progress *= 10000;
+			progress = Math.round(progress);
+			progress /= 100;
+
+			let speed = this.humanSpeed(this.stats.speed);
+
+			let timeLeft = this.parseMillisecondsIntoReadableTime(this.stats.eta);
+			text += `
+			<div class="input">
+				<p class="description" color="red">${progress}% | ${speed} | ${timeLeft}</p>
+				<progress value="${progress}" max="100"></progress>
+			</div>
+			`;
+		}
+
+		return text;
+	}
+
+	postRender() {
+		// adds click events for the file and directory inputs
+
+		const filePath = document.getElementById(`filePath-${this.uuid}`);
+		const directory = document.getElementById(`directory-${this.uuid}`);
+
+		filePath.addEventListener('click', async (event) => {
+			try {
+				console.log('getting file');
+				this.state.filePath = await this.getFileFromUser();
+				this.state.files = this.state.filteredFiles = [];
+				this.onLoad();
+			} catch (error) {}
+			this.rerenderModifiers();
+		});
+
+		directory.addEventListener('click', async (event) => {
+			try {
+				console.log('getting folder');
+				this.state.destination = await this.getDirectoryFromUser();
+				this.onLoad();
+			} catch (error) {}
+			this.rerenderModifiers();
+		});
+	}
+
 	humanSpeed(bits, si = false, dp = 1) {
+		// converts bits per second to something easy to read
 		const thresh = si ? 1000 : 1024;
 
 		if (Math.abs(bits) < thresh) {
@@ -241,7 +175,7 @@ class TorrentModifier extends Modifier {
 
 		const units = si
 			? ['Kb/s', 'Mb/s', 'Gbs', 'Tbs', 'Pbs', 'Eb/s', 'Zb/s', 'Yb/s']
-			: ['Kib/s', 'Mib/s', 'Gib/s', 'Tib/s', 'Pib/s', 'Eib/s', 'Zib/s', 'Yib/s'];
+			: ['Kb/s', 'Mb/s', 'Gb/s', 'Tb/s', 'Pb/s', 'Eb/s', 'Zb/s', 'Yb/s'];
 		let u = -1;
 		const r = 10 ** dp;
 
@@ -254,6 +188,7 @@ class TorrentModifier extends Modifier {
 	}
 
 	parseMillisecondsIntoReadableTime(milliseconds) {
+		// converts milliseconds to a time that is easy to read
 		if (milliseconds == Infinity) return 'Infinity';
 		//Get hours from milliseconds
 		var hours = milliseconds / (1000 * 60 * 60);
@@ -272,4 +207,37 @@ class TorrentModifier extends Modifier {
 
 		return h + ':' + m + ':' + s;
 	}
+
+	// OTHER
+	// OTHER
+	async onLoad() {
+		// when the modifier is loaded check for the torrent files
+
+		if (this.state.filePath === '' || this.state.destination === '') return;
+
+		try {
+			const client = new WebTorrent();
+			const torrentString = fs.readFileSync(this.state.filePath, {
+				path: this.state.destination,
+			});
+			const torrent = await client.add(torrentString, { path: this.state.destination });
+			console.log('this is the torrent', torrent);
+			this.state.files = torrent.files.map((v) =>
+				path.join(this.state.destination, v.path)
+			);
+			console.log(this.state.files);
+			client.destroy();
+		} catch (error) {}
+
+		this.rerenderModifiers();
+	}
+
+	// stores the stats
+	// not in state because it is data we don't want to save
+	stats = {
+		inProgress: false,
+		progress: 0,
+		speed: 0,
+		eta: 0,
+	};
 }
