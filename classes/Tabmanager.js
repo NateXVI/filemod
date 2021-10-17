f1a77476e63c8e08744414ed92556f632a74943e
Nate <natediven1@gmail.com>
2021-10-17 04:41:09 -0600

    Update Tabmanager.js

--- classes/Tabmanager.js
+++ classes/Tabmanager.js
@@ -11,6 +11,7 @@ class TabManager {
 		this.dropButtons = document.getElementsByClassName('dropbtn');
 		this.tabSection = document.getElementById('tabs');
 		this.dropdownContent.hidden = true;
+		this.addTabButton = document.getElementById('add-tab-button');
 
 		for (let i = 0; i < this.dropButtons.length; i++) {
 			let btn = this.dropButtons[i];
@@ -29,6 +30,20 @@ class TabManager {
 			if (event.target.id != 'add-button') this.dropdownContent.hidden = true;
 		});
 
+		this.modifyButton.addEventListener('click', () => this.modify());
+
+		this.addTabButton.addEventListener('click', () => this.newTab());
+
+		this.directoryText.addEventListener('click', async (event) => {
+			if (this.state.tabs[this.tab]) {
+				try {
+					let newDir = await this.getDirectoryFromUser();
+					this.state.tabs[this.tab].setDirectory(newDir);
+					event.target.innerHTML = newDir;
+				} catch (error) {}
+			}
+		});
+
 		this.tab = 0;
 
 		this.state = {
@@ -41,40 +56,44 @@ class TabManager {
 		};
 	}
 
+	modify(tabNumber = this.tab) {
+		if (this.state.tabs[tabNumber]) {
+			console.log('modify', tabNumber);
+			this.state.tabs[tabNumber].modify();
+		}
+	}
+
 	renderTabs() {
 		let view = '';
 		for (let i = 0; i < this.state.tabs.length; i++) {
 			view += `
-			<div class="tab" id="tab-${i}">
+			<div class="${i == this.tab ? 'tab-highlight ' : ''}tab" id="tab-${i}">
 				<p>Tab ${i + 1}</p>
-				<button id="remove-tab-${i}" class="close-tab-button">X</button>
+				<button id="close-tab-${i}" class="close-tab-button">X</button>
 			</div>
 			`;
-
-			let tabElements = document.getElementsByClassName('tab');
-			console.log(tabElements);
-
-			for (let i in this.state.tabs) {
-				console.log(`tab-${i}`);
-				// document.getElementById(`tab-${i}`).addEventListener('click', (event) => {
-				// 	this.switchTab(i);
-				// });
-			}
 		}
 		this.tabSection.innerHTML = view;
 
 		for (let i = 0; i < this.state.tabs.length; i++) {
-			// let tabButton = document.getElementById(`tab-${i}`);
-			// tabButton.addEventListener('click', (event) => {
-			// 	let tabNumber = event.target.id.split('-')[1];
-			// 	this.switchTab(tabNumber);
-			// });
+			let tabButton = document.getElementById(`tab-${i}`);
+			tabButton.addEventListener('click', (event) => {
+				let tabNumber = event.target.id.split('-')[1];
+				this.switchTab(tabNumber);
+			});
+
+			let closeTabButton = document.getElementById(`close-tab-${i}`);
+			closeTabButton.addEventListener('dblclick', (event) => {
+				let tabNumber = event.target.id.split('-')[2];
+				this.removeTab(tabNumber);
+			});
 		}
 	}
 
 	renderModifiers(tabNumber = this.tab) {
 		if (this.state.tabs[tabNumber]) {
 			this.modifiersSection.innerHTML = this.state.tabs[tabNumber].renderModifiers();
+			this.state.tabs[tabNumber].postRender();
 
 			// add events for buttons
 			const removeButtons = document.getElementsByClassName('remove-button');
@@ -119,14 +138,33 @@ class TabManager {
 		} catch (error) {}
 	}
 
+	renderAll() {
+		this.renderTabs();
+		this.renderModifiers();
+		this.renderPreview();
+	}
+
 	addModifier(type, tabNumber = this.tab) {
 		this.state.tabs[tabNumber].addModifier(type);
 	}
 
 	removeTab(tabNumber) {
-		if (!this.state.tabs[tabNumber]) return;
+		if (!this.state.tabs[tabNumber] || this.state.tabs.length <= 1) return;
 
 		this.state.tabs.splice(tabNumber, 1);
+
+		this.tab = Math.min(Math.max(this.tab, 0), this.state.tabs.length - 1);
+
+		this.renderTabs();
+		this.renderModifiers();
+		this.renderPreview();
+	}
+
+	newTab() {
+		this.state.tabs.push(new Filemod());
+		this.tab = this.state.tabs.length - 1;
+
+		this.renderAll();
 	}
 
 	switchTab(tabNumber) {
@@ -135,5 +173,14 @@ class TabManager {
 		this.tab = tabNumber;
 		this.renderTabs();
 		this.renderModifiers();
+	}
+
+	async getDirectoryFromUser() {
+		// triggers file dialog for user to select a folder and returns path of it
+		// so far it has only worked on Window
+		// tested on MacOS but it did not work
+		console.log('getting directory');
+		let val = await fileDialog({ type: 'directory' });
+		return val[0];
 	}
 }
