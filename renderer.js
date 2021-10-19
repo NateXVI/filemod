d2fd9d274626b467d9ddf5a5cf0f2023fcc54cc5
Nate <natediven1@gmail.com>
2021-10-19 02:23:26 -0600

    implemented load and save functions
    calls load function at start
    
    new event listener for render preview
    
    added intervals for rendering preview and saving

--- renderer.js
+++ renderer.js
@@ -1,7 +1,15 @@
 const tabs = new TabManager();
 
-tabs.renderModifiers();
-tabs.renderTabs();
+tabs.load();
+tabs.renderAll();
+
 document.addEventListener('render modifiers', () => {
 	tabs.renderModifiers();
 });
+
+document.addEventListener('render preview', () => {
+	tabs.renderPreview();
+});
+
+let previewInterval = setInterval(() => tabs.renderPreview(), 1000);
+let saveInterval = setInterval(() => tabs.save(), 10000);
