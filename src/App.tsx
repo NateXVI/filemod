415627d5f1309fd89740f6c6a369e5c35bb7feea
Nate Diven <natediven1@gmail.com>
2023-05-03 17:46:01 -0600

    created a temporary load example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,83 @@
+import { useEffect, useState } from "react";
+import { open } from "@tauri-apps/api/dialog";
+
+import { load } from "@/lib/commands";
+
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <div className="container"></div>;
+  const [folder, setFolder] = useState<string | null>(null);
+  const [nonRecursiveFiles, setNonRecursiveFiles] = useState<string[]>([]);
+  const [recursiveFiles, setRecursiveFiles] = useState<string[]>([]);
+  const [errors, setErrors] = useState<string[]>([]);
+
+  const addError = (error: string) => {
+    setErrors((errors) => [...errors, error]);
+  };
+
+  useEffect(() => {
+    if (folder === null) {
+      setNonRecursiveFiles([]);
+      setRecursiveFiles([]);
+      return;
+    }
+
+    load({
+      path: folder,
+      recursive: false,
+    })
+      .then(setNonRecursiveFiles)
+      .catch(addError);
+    load({
+      path: folder,
+      recursive: true,
+    })
+      .then(setRecursiveFiles)
+      .catch(addError);
+  }, [folder]);
+
+  const handleSelectFolder = async () => {
+    const selected = await open({
+      multiple: false,
+      directory: true,
+    });
+    if (selected === null) return;
+    setFolder(selected as string);
+  };
+
+  return (
+    <div
+      className="container flex flex-col gap-5 p-4"
+      style={{ fontFamily: "sans-serif" }}
+    >
+      <Button onClick={handleSelectFolder}>Select folder</Button>
+      <p>{folder ?? "No folder selected"}</p>
+      <details>
+        <summary>Errors</summary>
+        <ul>
+          {errors.map((error, index) => (
+            <li key={index}>{error}</li>
+          ))}
+        </ul>
+      </details>
+      {folder !== null && (
+        <>
+          <h1>Non recursive</h1>
+          <ul>
+            {nonRecursiveFiles.map((file, index) => (
+              <li key={index}>{file}</li>
+            ))}
+          </ul>
+          <h1>Recursive</h1>
+          <ul>
+            {recursiveFiles.map((file, index) => (
+              <li key={index}>{file}</li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
