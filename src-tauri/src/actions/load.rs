f22905dff6631bca60caf841fddff96add1a7a4d
Nate Diven <natediven1@gmail.com>
2023-05-16 01:56:20 -0600

    updated actions module

--- src-tauri/src/actions/load.rs
+++ src-tauri/src/actions/load.rs
@@ -1,91 +1,97 @@
-use std::{collections::HashSet, fs::read_dir};
-use tauri;
+use anyhow::Result;
+use serde::Deserialize;
+use std::fs::read_dir;
 use walkdir::WalkDir;
 
-#[derive(Debug)]
-pub enum Error {
-    Io(std::io::Error),
-    WalkDir(walkdir::Error),
+use super::{Extras, Files, RunAction};
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Load {
+    pub path: String,
+    pub recursive: bool,
 }
 
-impl serde::ser::Serialize for Error {
-    fn serialize<S: serde::ser::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        // serializer.serialize_str(&self);
-        match &self {
-            Error::Io(e) => serializer.serialize_str(&e.to_string()),
-            Error::WalkDir(e) => serializer.serialize_str(&e.to_string()),
-        }
-    }
-}
-
-#[tauri::command]
-pub fn load(
-    path: String,
-    recursive: bool,
-    files: Option<HashSet<String>>,
-) -> Result<HashSet<String>, Error> {
-    let mut files = files.unwrap_or(HashSet::new());
-    match recursive {
-        true => {
-            for entry in WalkDir::new(path) {
-                let entry = entry.map_err(|e| Error::WalkDir(e))?;
-                let path = entry.path();
-                if path.is_file() {
-                    files.insert(path.to_str().unwrap().to_string());
-                }
-            }
-            Ok(files)
-        }
-        false => {
-            let entries = read_dir(path).map_err(|e| Error::Io(e))?;
-            entries.for_each(|entry| match entry {
-                Ok(entry) => {
+impl RunAction for Load {
+    fn run(&self, files: Files, _: Option<Extras>) -> Result<Files> {
+        let mut files = files;
+        match self.recursive {
+            true => {
+                for entry in WalkDir::new(&self.path) {
+                    let entry = entry?;
                     let path = entry.path();
                     if path.is_file() {
-                        files.insert(path.to_str().unwrap().to_string());
+                        files.insert(path.to_path_buf());
                     }
                 }
-                _ => {}
-            });
-            Ok(files)
+                Ok(files)
+            }
+            false => {
+                let entries = read_dir(&self.path)?;
+                entries.for_each(|entry| match entry {
+                    Ok(entry) => {
+                        let path = entry.path();
+                        if path.is_file() {
+                            files.insert(path);
+                        }
+                    }
+                    _ => {}
+                });
+                Ok(files)
+            }
         }
+    }
+    fn preview(&self, files: Files) -> Result<Files> {
+        return Self::run(&self, files, None);
     }
 }
 
 #[cfg(test)]
-const TEST_PATH: &str = "./test-files/load";
+const TEST_LOAD_PATH: &str = "./test-files/load";
 
 #[test]
 fn test_load_not_recursive() {
-    let expected_files: HashSet<String> =
-        vec!["./test-files/load/file1.txt", "./test-files/load/file2.txt"]
-            .into_iter()
-            .map(|s| s.to_string().replace("\\", "/"))
-            .collect();
+    use std::{collections::HashSet, path::PathBuf};
 
-    let files: HashSet<String> = load(TEST_PATH.to_string(), false, None)
+    let action = Load {
+        path: TEST_LOAD_PATH.to_string(),
+        recursive: false,
+    };
+    let expected_files: Files = vec!["./test-files/load/file1.txt", "./test-files/load/file2.txt"]
+        .into_iter()
+        .map(|s| s.to_string().replace("\\", "/"))
+        .map(|s| PathBuf::from(s))
+        .collect();
+
+    let files: Files = action
+        .run(HashSet::new(), None)
         .unwrap()
         .into_iter()
-        .map(|s| s.replace("\\", "/"))
         .collect();
     assert_eq!(files, expected_files);
 }
 
 #[test]
 fn test_load_recursive() {
-    let expected_files: HashSet<String> = vec![
+    use std::{collections::HashSet, path::PathBuf};
+    let action = Load {
+        path: TEST_LOAD_PATH.to_string(),
+        recursive: true,
+    };
+
+    let expected_files: Files = vec![
         "./test-files/load/file1.txt",
         "./test-files/load/file2.txt",
         "./test-files/load/subdir/file3.txt",
     ]
     .into_iter()
     .map(|s| s.to_string().replace("\\", "/"))
+    .map(|s| PathBuf::from(s))
     .collect();
 
-    let files: HashSet<String> = load(TEST_PATH.to_string(), true, None)
+    let files: Files = action
+        .run(HashSet::new(), None)
         .unwrap()
         .into_iter()
-        .map(|s| s.replace("\\", "/"))
         .collect();
 
     assert_eq!(files, expected_files);
--- src-tauri/src/actions/mod.rs
+++ src-tauri/src/actions/mod.rs
@@ -1,1 +1,91 @@
+use anyhow::Result;
+use enum_dispatch::enum_dispatch;
+use serde::Deserialize;
+use std::{collections::HashSet, path::PathBuf};
+
 pub mod load;
+
+pub type File = PathBuf;
+pub type Files = HashSet<File>;
+
+#[enum_dispatch]
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(tag = "type", rename_all = "camelCase")]
+pub enum Action {
+    Load(load::Load),
+}
+
+#[enum_dispatch(Action)]
+pub trait RunAction {
+    fn run(&self, files: Files, extras: Option<Extras>) -> Result<Files>;
+    fn preview(&self, files: Files) -> Result<Files>;
+}
+
+// extras are only used for tauri actions
+pub struct Extras {
+    window: tauri::Window,
+}
+
+pub struct Pipeline {
+    pub actions: Vec<Action>,
+}
+
+impl Pipeline {
+    pub fn run(&self) -> Result<Files> {
+        let mut files = Files::new();
+        for action in &self.actions {
+            files = action.run(files, None)?;
+        }
+        Ok(files)
+    }
+    pub fn preview(&self, files: Files) -> Result<Files> {
+        let mut files = files;
+        for action in &self.actions {
+            files = action.preview(files)?;
+        }
+        Ok(files)
+    }
+}
+
+#[test]
+fn test_pipeline() {
+    let pipeline = Pipeline {
+        actions: vec![Action::Load(load::Load {
+            path: "./test-files/load".into(),
+            recursive: false,
+        })],
+    };
+
+    let files = pipeline.run().unwrap();
+    println!("{:?}", files);
+    assert_eq!(
+        files,
+        vec!["./test-files/load/file1.txt", "./test-files/load/file2.txt"]
+            .into_iter()
+            .map(|f| f.into())
+            .collect()
+    )
+}
+
+#[test]
+fn test_deserialize_actions() {
+    let actions: Vec<Action> = serde_json::from_str(
+        r#"
+        [
+            {
+                "type": "load",
+                "path": "./test-files/load",
+                "recursive": false
+            }
+        ]
+        "#,
+    )
+    .unwrap();
+    assert_eq!(
+        actions,
+        vec![Action::Load(load::Load {
+            path: "./test-files/load".into(),
+            recursive: false,
+        })]
+    );
+}
