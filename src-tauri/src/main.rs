ba6cd058a5e88fd1e90183176f26da38dd4779f1
Nate Diven <natediven1@gmail.com>
2023-05-16 20:16:54 -0600

    updated main

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,7 @@
 mod actions;
 mod pipeline;
+use actions::Files;
+use pipeline::Pipeline;
 use tauri_plugin_window_state;
 
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
@@ -9,7 +11,20 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_persisted_scope::init())
-        .invoke_handler(tauri::generate_handler![actions::load::load])
+        .invoke_handler(tauri::generate_handler![execute_pipeline])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+async fn execute_pipeline(
+    // app: tauri::AppHandle,
+    window: tauri::Window,
+    pipeline: Pipeline,
+) -> Result<Files, String> {
+    let extras = actions::Extras {
+        id: pipeline.id.clone(),
+        window,
+    };
+    pipeline.run(Some(extras)).map_err(|e| e.to_string())
 }
